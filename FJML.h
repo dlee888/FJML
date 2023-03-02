2c791e0ec584e4ab2e9ab3056e4313395e20321e
jeffarjeffar <jeffarjeffar@gmail.com>
2023-03-02 10:25:33 -0800

    fix

--- FJML.h
+++ FJML.h
@@ -4,11 +4,10 @@
 #include "./FJML/activations/activations.h"
 #include "./FJML/data/data.h"
 #include "./FJML/layers/layers.h"
+#include "./FJML/linalg/linalg.h"
+#include "./FJML/linalg/tensor.h"
 #include "./FJML/loss/loss.h"
 #include "./FJML/mlp/mlp.h"
 #include "./FJML/optimizers/optimizers.h"
-#include "./FJML/util/linalg.h"
-#include "./FJML/util/types.h"
-#include "./FJML/util/util.h"
 
 #endif
